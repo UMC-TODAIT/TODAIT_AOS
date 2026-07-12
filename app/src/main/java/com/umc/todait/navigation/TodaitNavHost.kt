4f61bff2215ff297cdcb2b0a0d62bb9a11bc6a26
Jihee <selfishgene0523@naver.com>
2026-07-12 11:12:31 +0900

    fix: 시작 페이지 위치 오류 수정

--- app/src/main/java/com/umc/todait/navigation/TodaitNavHost.kt
+++ app/src/main/java/com/umc/todait/navigation/TodaitNavHost.kt
@@ -61,7 +61,7 @@ fun TodaitApp() {
     ) { innerPadding ->
         NavHost(
             navController = navController,
-            startDestination = Screen.MyPage.route,//Login으로 되돌리세요
+            startDestination = Screen.Login.route,
             modifier = Modifier.padding(innerPadding),
         ) {
             // ---------- Auth ----------

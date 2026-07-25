fda4ddc581c0989ee40ab0dcfe867cc284c1cf74
Jihee <selfishgene0523@naver.com>
2026-07-25 19:03:02 +0900

    fix: TodaitNavHost 수정

--- app/src/main/java/com/umc/todait/navigation/TodaitNavHost.kt
+++ app/src/main/java/com/umc/todait/navigation/TodaitNavHost.kt
@@ -98,7 +98,7 @@ fun TodaitApp() {
     ) { innerPadding ->
         NavHost(
             navController = navController,
-            startDestination =  Screen.MyPage.route,
+            startDestination =  Screen.Login.route,
             modifier = Modifier.padding(innerPadding),
         ) {
             // ---------- Auth ----------
